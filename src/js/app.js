f36a3618521985d48a9fc7e29ed4407d8c9a10fa
Denis Michailov <kamrad.reg@gmail.com>
2016-07-14 22:20:39 +1000

    Spaghetti code - start point

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,31 @@ var $c_01 = '#21b1e0',
 	$c_03 = '#1d7e6b';
 
 console.log( "D3 version: " + d3.version );
+
+// -------------------------------------------------------------
+
+var people = [];
+var template = $('#peopleTemplate').html();
+
+$('#peopleModule').find('button').on('click', function() {
+	if ($('#peopleModule').find('input').val() !== ''){
+		
+		people.push($('#peopleModule').find('input').val());
+		$('#peopleModule').find('input').val('');
+		
+		var data = {
+			people: people
+		};
+
+		$('#peopleModule').find('ul').html(Mustache.render(template, data));	
+	}
+});
+
+$('#peopleModule').find('ul').delegate('i.del', 'click', function(e) {
+	
+	var $remove = $(e.target).closest('li');
+	var i = $('#peopleModule').find('ul').find('li').index($remove);
+
+	$remove.remove();
+	people.splice(i, 1);
+});
